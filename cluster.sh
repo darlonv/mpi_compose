05ed905ca79fc4dc1ee4b863eaeec78f922f5a53
Darlon <darlonv@gmail.com>
2022-06-10 16:12:50 -0300

    Included -e , to execute a bash on master node

--- cluster.sh
+++ cluster.sh
@@ -4,22 +4,22 @@ NP=10
 
 #Cluster up
 cluster_up(){
-	echo "**** Starting cluster ***"
+	echo "**** Starting cluster ****"
 	docker-compose up -d --scale worker="${1}"
 }
 
 #Cluster down
 cluster_stop(){
-	echo "**** Stopping cluster ***"
+	echo "**** Stopping cluster ****"
 	docker-compose stop
-	echo "**** Cluster down ***"
+	echo "**** Cluster down ****"
 }
 
 #Cluster test
 cluster_test(){
-	echo "**** Executing test ***"
+	echo "**** Executing test ****"
 	./mpi_test.sh -c -n "${1}"
-	echo "**** Test done ***"
+	echo "**** Test done ****"
 }
 
 cluster_status(){
@@ -46,14 +46,18 @@ reset_ssh_keys(){
 		docker exec "$WORKER_NAME" chown -R mpi.mpi /home/mpi/.ssh
 	done
 	rm -r .ssh
+	
+	#Update ips list
+	./get_ips.sh
 }
 
-while getopts n:hst:ur flag
+while getopts n:hst:ure flag
 do
 	case "${flag}" in
 		h)
 			echo "${0}" '[-h|-n <np>]'
 			echo "	-h : this help menu."
+			echo "  -e : execute a bash on Master node."
 			echo "	-r : set the cluster size and generate new ssh keys."
 			echo "	-s : stop the cluster."
 			echo "	-t <np>: execute cluster test with np processes."
@@ -61,12 +65,19 @@ do
 			echo "	-n <np> : set the number of containers to configure for the cluster. Default: 10."
 			exit 0
 			;;
+		e)
+			#Geting master container id
+	MASTER_ID=$(awk '{print $1}' <(docker-compose ps | egrep master))
+			docker exec --user mpi -w '/home/mpi' -it "${MASTER_ID}" bash 
+			;;
 		n) 
-			NP="${OPTARG}" 
+			NP="${OPTARG}"
+			cluster_up "${NP}"
 			;;
 		r)
 			cluster_up "${NP}"
 			reset_ssh_keys
+			cluster_stop
 			exit 0
 			;;
 		t)
@@ -88,7 +99,7 @@ do
 	esac
 done
 
-if [ -eq 0 ]
+if [ $# -eq 0 ]
 then
 	cluster_up "${NP}"
 fi
